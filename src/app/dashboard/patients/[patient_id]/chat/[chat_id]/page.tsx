450255d5993673ee3aa7c61cea31a829f9ea1d5e
Muhamad Ilyas Mustafa <emailpribadiilyas@gmail.com>
2024-11-03 18:01:44 +0700

    add submit refine

--- src/app/dashboard/patients/[patient_id]/chat/[chat_id]/page.tsx
+++ src/app/dashboard/patients/[patient_id]/chat/[chat_id]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useCallback, useEffect, useState } from "react";
-import { generateDraftMedRec } from "./actions";
+import { generateDraftMedRec, refineSummarization } from "./actions";
 import { blobToBase64 } from "@/app/utils/blob-to-base-64";
 import { useVoiceVisualizer, VoiceVisualizer } from "react-voice-visualizer";
 import Link from "next/link";
@@ -12,9 +12,35 @@ import { Textarea } from "@/components/ui/textarea";
 import { Prompt } from "@/app/types";
 import ReactMarkdown from "react-markdown";
 import { useFormStatus } from "react-dom";
+import { useForm } from "react-hook-form";
+
+const dummyPrompts: Prompt[] = [
+  {
+    id: "1",
+    content: "Hello, how can I help you today?",
+    sender: "bot",
+  },
+  {
+    id: "2",
+    content: "I'm feeling unwell",
+    sender: "user",
+  },
+  {
+    id: "3",
+    content: "Can you describe your symptoms?",
+    sender: "bot",
+  },
+];
 
 export default function ChatRoom() {
-  const [prompts, setPrompts] = useState<Prompt[]>([]);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    formState: { errors },
+  } = useForm();
+  const [prompts, setPrompts] = useState<Prompt[]>(dummyPrompts);
   const { pending } = useFormStatus();
   // Initialize the recorder controls using the hook
   const recorderControls = useVoiceVisualizer();
@@ -30,6 +56,29 @@ export default function ChatRoom() {
       setPrompts((prevPrompts) => [...prevPrompts, prompt]);
     },
     [setPrompts]
+  );
+
+  const submitMessage = useCallback(
+    async (data) => {
+      try {
+        const nextPrompt: Prompt = {
+          id: crypto.getRandomValues(new Uint32Array(1))[0].toString(),
+          content: data.message,
+          sender: "user",
+        };
+        setValue("message", "");
+        addPrompt(nextPrompt);
+        const response = await refineSummarization([...prompts, nextPrompt]);
+        addPrompt({
+          id: crypto.getRandomValues(new Uint32Array(1))[0].toString(),
+          content: response.join("\n"),
+          sender: "bot",
+        });
+      } catch (error) {
+        console.error("Error processing message:", error);
+      }
+    },
+    [addPrompt, prompts, setValue]
   );
 
   const processAudio = useCallback(
@@ -95,9 +144,17 @@ export default function ChatRoom() {
           ? prompts.map((prompt, index) => (
               <div
                 key={index}
-                className="p-2 bg-white border border-input rounded-lg my-2"
+                className={cn(
+                  "my-2 w-fit max-w-[90%]",
+                  prompt.sender === "user" ? "ml-auto" : "mr-auto"
+                )}
               >
-                <ReactMarkdown>{prompt.content}</ReactMarkdown>
+                <span>{prompt.sender === "bot" ? "Bot" : "You"} says:</span>
+                <div
+                  className={cn("p-2 bg-primary-foreground rounded-lg mt-2")}
+                >
+                  <ReactMarkdown>{prompt.content}</ReactMarkdown>
+                </div>
               </div>
             ))
           : null}
@@ -109,11 +166,13 @@ export default function ChatRoom() {
           "transition-transform duration-500",
           recordedBlob ? "translate-y-0" : "translate-y-40"
         )}
+        onSubmit={handleSubmit(submitMessage)}
       >
         <Textarea
           placeholder="Send your message here"
           className="flex-grow h-3 resize-none"
           rows={1}
+          {...register("message")}
         />
         <Button type="submit">
           <SendHorizontalIcon />
