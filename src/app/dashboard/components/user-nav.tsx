96600e47c6e6760bb40e770d338059fdb7cd0ee3
Muhamad Ilyas Mustafa <emailpribadiilyas@gmail.com>
2024-11-03 10:16:11 +0700

    remove unused component

--- src/app/dashboard/components/user-nav.tsx
+++ src/app/dashboard/components/user-nav.tsx
@@ -17,17 +17,17 @@ export function UserNav() {
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-9 w-9">
-            <AvatarImage src="/avatars/03.png" alt="@shadcn" />
-            <AvatarFallback>SC</AvatarFallback>
+            <AvatarImage src="/avatars/03.png" alt="@caresync" />
+            <AvatarFallback>CS</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56" align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">shadcn</p>
+            <p className="text-sm font-medium leading-none">Caresync</p>
             <p className="text-xs leading-none text-muted-foreground">
-              m@example.com
+              dev@caresync.ai
             </p>
           </div>
         </DropdownMenuLabel>
